4087144eb39afb233141d4f602ed4d2613764498
tannv_57 <NVantan94@gmail.com>
2020-08-31 11:15:13 +0900

    initial commit

---
+++ src/main/java/com/tannv/springcicdsample/SpringCiCdSampleApplication.java
@@ -0,0 +1,13 @@
+package com.tannv.springcicdsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiCdSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCiCdSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tannv/springcicdsample/SpringCiCdSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tannv.springcicdsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCiCdSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
