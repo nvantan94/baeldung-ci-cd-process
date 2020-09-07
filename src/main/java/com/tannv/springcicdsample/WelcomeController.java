729b3ae8521298f4bc7870e2e8b20f237af1e7ad
tannv_57 <NVantan94@gmail.com>
2020-09-07 17:11:43 +0900

    add cd task for travis ci

---
+++ src/main/java/com/tannv/springcicdsample/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.tannv.springcicdsample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @RequestMapping("/welcome")
+    public String welcomeText() {
+        return "welcome";
+    }
+}
